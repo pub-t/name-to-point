15a16919ba2937495de7e15d7384dbe922682d01
Shuba Maxim <pro100stalkerss@gmail.com>
2016-06-14 14:38:20 +0300

    add request in osm/search

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 var http = require('http');
 var app = express();
 var bodyParser = require('body-parser');
@@ -6,7 +7,11 @@ var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.post('/api', urlencodedParser, function (req, res) {
-    res.sendFile(__dirname + "/" + 'example.json');
+    request({url: 'https://nominatim.openstreetmap.org/search/' + req.body.name + '?format=json'}, function (err, response,data) {
+        if(err) return next(err);
+        console.log(data);
+        res.send(data);
+    });
 });
 
 var server = http.createServer(app);
