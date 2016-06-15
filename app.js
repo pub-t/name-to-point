7f0a4fb9370002ead50ecf3a23dd0239af69825a
Shuba Maxim <pro100stalkerss@gmail.com>
2016-06-15 15:57:24 +0300

    remove long URL

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-var express = require('express');
-var request = require('request');
-var http = require('http');
-var app = express();
-var bodyParser = require('body-parser');
+require('babel-register');
+const express = require('express');
+const request = require('request');
+const http = require('http');
+const app = express();
+const bodyParser = require('body-parser');
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.post('/api', urlencodedParser, function (req, res) {
-    request({url: 'https://nominatim.openstreetmap.org/search/' + req.body.name + '?format=json'}, function (err, response,data) {
-        if(err) return next(err);
-        console.log(data);
-        res.send(data);
-    });
+app.post('/serchСoordinate', urlencodedParser, (req, res, next) => {
+  const searchURL = `https://nominatim.openstreetmap.org/search/${req.body.name}?format=json`;
+  request({ url: searchURL }, function (err, response, data) {
+    if (err) return next(err);
+    res.send(data);
+  });
 });
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 
 server.listen(7788);
 
