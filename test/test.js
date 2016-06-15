60d05b2f39676c132df84e42522c1f8bb9351c77
Shuba Maxim <pro100stalkerss@gmail.com>
2016-06-15 15:56:06 +0300

    refactoring code with eslint

--- test/test.js
+++ test/test.js
@@ -1,17 +1,13 @@
-var request = require('supertest');
-var app = require('../app');
-describe("transforms the name of the object in a coordinate", function (){
-
-    it("Input place name-string,base coordinate, return coordinate and full name point", function(done){
-
-        request(app)
-            .post("localhost:7788/serchСoordinate")
-            .send({SearchString:'Oldcity', BaseСoordinate: {lat: "53.93892", lon: "28.6496"}}) //текс + координата базовая
+const request = require('supertest');
+const app = require('../app');
+describe('transforms the name of the object in a coordinate', () => {
+  it('Input place name-string,base coordinate, return coordinate and full name point', (done) => {
+    request(app)
+            .post('localhost:7788/serchСoordinate')
+            .send({ SearchString: 'Oldcity', BaseСoordinate: { lat: '53.93892', lon: '28.6496' } })
             .expect(200)
-            .expect({lat: "53.70177645", lon: "23.8347894179425",
-                display_name: "OldCity, 17, улица Дубко, Девятовка, Ленинский район, Hrodna, Hrodna region, 230012, Belarus"});
-            done();
-        
-    });
-    
+            .expect({ lat: '53.70177645', lon: '23.8347894179425', display_name: 'OldCity, 17, ' +
+            'улица Дубко, Девятовка, Ленинский район, Hrodna, Hrodna region, 230012, Belarus' });
+    done();
+  });
 });
