4e6fe6ba2e6a596f5a2f7823793101e9b9f42e0b
Shuba Maxim <pro100stalkerss@gmail.com>
2016-06-16 08:31:07 +0300

    update code with eslint

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-require('babel-register');
 const express = require('express');
 const request = require('request');
 const http = require('http');
 const app = express();
 const bodyParser = require('body-parser');
+const nconf = require('./config');
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.post('/serchСoordinate', urlencodedParser, (req, res, next) => {
-  const searchURL = `https://nominatim.openstreetmap.org/search/${req.body.name}?format=json`;
+app.post('/searchCoordinate', urlencodedParser, function (req, res, next) {
+  const searchURL = 'https://nominatim.openstreetmap.org/search/' + req.body.name + '?format=json';
   request({ url: searchURL }, function (err, response, data) {
     if (err) return next(err);
     res.send(data);
@@ -17,6 +17,6 @@ app.post('/serchСoordinate', urlencodedParser, (req, res, next) => {
 
 const server = http.createServer(app);
 
-server.listen(7788);
+server.listen(nconf.get('port'));
 
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,13 +1,15 @@
 const request = require('supertest');
 const app = require('../app');
-describe('transforms the name of the object in a coordinate', () => {
-  it('Input place name-string,base coordinate, return coordinate and full name point', (done) => {
-    request(app)
-            .post('localhost:7788/serchСoordinate')
-            .send({ SearchString: 'Oldcity', BaseСoordinate: { lat: '53.93892', lon: '28.6496' } })
+const nconf = require('../config');
+describe('transforms the name of the object in a coordinate', function () {
+  it('Input place name-string,base coordinate, return coordinate and full name point',
+      function (done) {
+        request(app)
+            .post(nconf.get('address') + '/serchСoordinate')
+            .send({ SearchString: 'Oldcity', BaseCoordinate: { lat: '53.93892', lon: '28.6496' } })
             .expect(200)
             .expect({ lat: '53.70177645', lon: '23.8347894179425', display_name: 'OldCity, 17, ' +
             'улица Дубко, Девятовка, Ленинский район, Hrodna, Hrodna region, 230012, Belarus' });
-    done();
-  });
+        done();
+      });
 });
