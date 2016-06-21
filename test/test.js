b2050020412faf7f8bb071e475ea41777225fb51
Shuba Maxim <pro100stalkerss@gmail.com>
2016-06-21 08:18:20 +0300

    add new expect && small changes

--- test/test.js
+++ test/test.js
@@ -1,15 +1,30 @@
 const request = require('supertest');
 const app = require('../app');
-const nconf = require('../config');
 describe('transforms the name of the object in a coordinate', function () {
   it('Input place name-string,base coordinate, return coordinate and full name point',
       function (done) {
         request(app)
-            .post(nconf.get('address') + '/serchСoordinate')
-            .send({ SearchString: 'Oldcity', BaseCoordinate: { lat: '53.93892', lon: '28.6496' } })
-            .expect(200)
-            .expect({ lat: '53.70177645', lon: '23.8347894179425', display_name: 'OldCity, 17, ' +
-            'улица Дубко, Девятовка, Ленинский район, Hrodna, Hrodna region, 230012, Belarus' });
-        done();
+          .post('/searchCoordinate')
+          .send({
+            location: {
+              accuracy: 40,
+              lat: '53.663482',
+              lon: '23.834427',
+            },
+            to: 'ул. Лиможа 27/1',
+          })
+          .expect(200)
+          .expect({
+            lat: '53.70177645',
+            lon: '23.8347894179425',
+            display_name: 'OldCity, 17, улица Дубко, Девятовка, Ленинский район, ' +
+            'Hrodna, Hrodna region, 230012, Belarus',
+          })
+          .end(function (err, res) {
+            if (err) {
+              return done(err);
+            }
+            done();
+          });
       });
 });
