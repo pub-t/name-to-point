ac6f9b5df2c59b24f48e5b77559fdfc74d95b34e
Shuba Maxim <pro100stalkerss@gmail.com>
2016-06-30 15:52:36 +0300

    Add /GET request instead /POST in app.js,test && update test *add json.parse for parsing request *remove body-parser, because it is not needed *add new variables for store name and coordinates -Test *rewrite display_name

--- app.js
+++ app.js
@@ -2,31 +2,36 @@ const express = require('express');
 const request = require('request');
 const http = require('http');
 const app = express();
-const bodyParser = require('body-parser');
 const nconf = require('./config');
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-app.post('/searchCoordinate', urlencodedParser, function (req, res, next) {
-  const myBody = {
-    location: {
-      accuracy: 40,
-      lat: '53.663482',
-      lon: '23.834427',
-    },
-    to: 'ул. Лиможа 27/1',
-  };
-  const search = myBody.to + ',' + myBody.location.lat + ' ' + myBody.location.lon;
-  /* const searchName = req.body;*/
+app.get('/point', function (req, res, next) {
+  const myBody = req.query;
+  const search = myBody.name + ',' + myBody.lat + ' ' + myBody.lon;
   const searchURL = 'https://nominatim.openstreetmap.org/search?q=' + encodeURIComponent(search) +
     '&format=json';
+  let resultJSON;
+  let valueOfDisplayName;
+  let valueOfLat;
+  let valueOfLon;
   request({ url: searchURL }, function (err, response, data) {
     if (err) return next(err);
-    res.send({
-      lat: '53.70177645',
-      lon: '23.8347894179425',
-      display_name: 'OldCity, 17, улица Дубко, Девятовка, Ленинский район, Hrodna, Hrodna region, 230012, Belarus',
+    JSON.parse(data, (key, value) => {
+      if (key === 'display_name') {
+        valueOfDisplayName = value;
+      }
+      if (key === 'lat') {
+        valueOfLat = value;
+      }
+      if (key === 'lon') {
+        valueOfLon = value;
+      }
+      resultJSON = {
+        display_name: valueOfDisplayName,
+        lat: valueOfLat,
+        lon: valueOfLon,
+      };
     });
+    res.send(resultJSON);
   });
 });
 const server = http.createServer(app);
--- test/test.js
+++ test/test.js
@@ -4,21 +4,13 @@ describe('transforms the name of the object in a coordinate', function () {
   it('Input place name-string,base coordinate, return coordinate and full name point',
       function (done) {
         request(app)
-          .post('/searchCoordinate')
-          .send({
-            location: {
-              accuracy: 40,
-              lat: '53.663482',
-              lon: '23.834427',
-            },
-            name: 'ул. Лиможа 27/1',
-          })
+          .get('/point?name=OldCity&lat=53.66&lon=23.83')
           .expect(200)
           .expect({
             lat: '53.70177645',
             lon: '23.8347894179425',
-            display_name: '27/1, улица Лиможа, Девятовка–4, Ленинский район, ' +
-            'Hrodna, Hrodna region, 230021, Belarus',
+            display_name: 'OldCity, 17, улица Дубко, Девятовка, Ленинский район,' +
+            ' Гродно, Гродненская область, 230012, Беларусь',
           })
           .end(function (err, res) {
             if (err) {
