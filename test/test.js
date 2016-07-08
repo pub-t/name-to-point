5d8b46932f22772a66cfc7e8f0ac1c6f935bcc5f
Shuba Maxim <pro100stalkerss@gmail.com>
2016-07-08 12:53:12 +0300

    Update indentation in test following airbnb rules

--- test/test.js
+++ test/test.js
@@ -2,23 +2,23 @@ const request = require('supertest');
 const app = require('../app');
 describe('name to point', function () {
   it('should return JSON object with geolocation',
-      function (done) {
-        request(app)
-          .get('/point?name=OldCity&lat=53.66&lon=23.83')
-          .expect(200)
-          .expect({
-            lat: '53.70177645',
-            lon: '23.8347894179425',
-            display_name: 'OldCity, 17, улица Дубко, Девятовка, Ленинский район,' +
-            ' Гродно, Гродненская область, 230012, Беларусь',
-          })
-          .end(function (err, res) {
-            if (err) {
-              return done(err);
-            }
-            done();
-          });
-      });
+    function (done) {
+      request(app)
+        .get('/point?name=OldCity&lat=53.66&lon=23.83')
+        .expect(200)
+        .expect({
+          lat: '53.70177645',
+          lon: '23.8347894179425',
+          display_name: 'OldCity, 17, улица Дубко, Девятовка, Ленинский район,' +
+          ' Гродно, Гродненская область, 230012, Беларусь',
+        })
+        .end(function (err, res) {
+          if (err) {
+            return done(err);
+          }
+          done();
+        });
+    });
   it('should respond with Not Found in case of failed name resolution',
     function (done) {
       request(app)
